8ee34d275d05fa0f10654a5e9b862ea900fa4e52
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-10 22:15:28 -0800

    Update AES-256-CBC-encrypt.php

--- Encryption/openSSL/AES-256-CBC-encrypt.php
+++ Encryption/openSSL/AES-256-CBC-encrypt.php
@@ -5,11 +5,11 @@ $data='TEXT OR FILES TO ENCRYPT';
 /*DEFINE CIPHER*/"\r\n"; 
 $cipher="aes-256-cbc"; 
 
-//Generate a 256-bit encryption key 
+/*CREATE A 256 BIT ENCRYPTION KEY*/"\r\n"; 
 $encryption_key=openssl_random_pseudo_bytes(32); 
 /*SAVE $encryption_key SOMEWHERE, LIKE IN A DB*/"\r\n";
 
-// Generate an initialization vector 
+/*GENERATE AN IV*/"\r\n"; 
 $iv_size=openssl_cipher_iv_length($cipher); 
 $iv=openssl_random_pseudo_bytes($iv_size); 
 /*SAVE $iv SOMEWHERE, LIKE IN A DB*/"\r\n";
