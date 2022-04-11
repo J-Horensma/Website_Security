71977f9867ec81e2043cffd360336e4c6b1127a5
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-11 15:53:14 -0800

    Update csp_reports.js

--- Content-Security-Policy/Reports/csp_reports.js
+++ Content-Security-Policy/Reports/csp_reports.js
@@ -1,6 +1,12 @@
-/*BY: JOEL HORENSMA*/"\r\n;
+/*BY: JOEL HORENSMA*/
+/*LAST UPDATED: APRIL 11/2022*/
 /*THIS SCRIPT COLLECTS A REPORT OF VIOLATED CSP POLICYS*/
-/*SCRIPT CAN BE CALLED WITH A SRC ATTRIBUTE*/
+/*THIS SCRIPT IS ABLE TO COLLECT DATA FROM ERROR OUTPUT, OF THE CSP.*/
+/*SCRIPT CAN BE CALLED WITH A "src" ATTRIBUTE, IN A SCRIPT.*/
+/*SCRIPT WILL NOT LOAD, FROM THE SAME PAGE, WHEN CSP IS SET UP, BECAUSE THAT VIOLATES STANDARD CSP.*/
+/*USAGE:*/
+/*CALL THIS SCRIPT, BEFORE OTHER SCRIPTS, SO IT CAN IDENTIFY PROBLEMS, BEFORE THE PAGE FAILS*/
+/*CALL REPORT WITH "violation_report" JS VARIABLE*/
 document.addEventListener("securitypolicyviolation", (e) => {
  var policyEnforced=e.disposition;
  var setPolicy=e.originalPolicy;
@@ -14,7 +20,7 @@ document.addEventListener("securitypolicyviolation", (e) => {
  var blockedResource=e.sample;
  var occurenceStatusCode=e.statusCode;
  
-var violation_report='Enforce or report?: ' + policyEnforced + '\n' +
+var violation_report='Enforced or report only?: ' + policyEnforced + '\n' +
 'Policy: ' + setPolicy + '\n' +
 'Violated policy: ' + violatedPolicy + '\n' +
 'Violation type: ' + blockedResourceType + '\n' +
@@ -25,5 +31,4 @@ var violation_report='Enforce or report?: ' + policyEnforced + '\n' +
 'Column of page violation: ' + violationColumnNumber + '\n' +
 'Blocked resource: ' + blockedResource + '\n' +
 'Occurrence status code: ' + occurenceStatusCode + '\n';
-alert(violation_report);
 });
