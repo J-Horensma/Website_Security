56098c52fca1f88855979aca57c7cac5ae380507
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-11 22:57:55 -0800

    Update AES-256-CBC-decrypt.php

--- Encryption/openSSL/Content_Encryption/Decrypt/AES-256-CBC-decrypt.php
+++ Encryption/openSSL/Content_Encryption/Decrypt/AES-256-CBC-decrypt.php
@@ -1,1 +1,17 @@
+<?php
+/*THIS SCRIPT DECRYPTS AES 256 BIT CBC ENCRYPTION WITH KEY AND IV, FROM ENCRYPTION PROCESS*/"\r\n";
+/*REF: https://www.phpcluster.com/aes-encryption-and-decryption-in-php/*/"\r\n";
+/*OPENSSL WORKS BY DEFAULT, IN MODERN PHP VERSIONS*/"\r\n";
+/*WORKS WITH USER INPUT VARIABLES AND FILES*/"\r\n";
+/*THE KEY, IV, AND ENCRYPTED DATA, ARE REQUIRED, BY THIS SCRIPT, SO IT CAN DECRYPT THE DATA*/"\r\n";
+$encryption_key='ENCRYPTION KEY';
+$iv='IV';
+$encrypted_data='ENCRYPTED DATA';
 
+/*DEFINE CIPHER*/"\r\n"; 
+$cipher = "aes-256-cbc"; 
+
+/*DECRYPT*/"\r\n"; 
+$decrypted_data=openssl_decrypt($encrypted_data, $cipher, $encryption_key, 0, $iv); 
+/*DO SOMETHING WITH $decrypted_data*/"\r\n";
+?>
