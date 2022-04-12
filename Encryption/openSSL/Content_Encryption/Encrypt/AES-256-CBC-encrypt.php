d8b898b174d3228d4a9287e3de17c1f21de62c4b
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-11 22:49:15 -0800

    Update AES-256-CBC-encrypt.php

--- Encryption/openSSL/Content_Encryption/Encrypt/AES-256-CBC-encrypt.php
+++ Encryption/openSSL/Content_Encryption/Encrypt/AES-256-CBC-encrypt.php
@@ -1,1 +1,22 @@
+<?php
+/*REF: https://www.phpcluster.com/aes-encryption-and-decryption-in-php/*/"\r\n";
+/*OPENSSL WORKS BY DEFAULT, IN MODERN PHP VERSIONS*/"\r\n";
+/*WORKS WITH USER INPUT VARIABLES AND FILES*/"\r\n";
+$data='TEXT OR FILE TO ENCRYPT';
 
+/*DEFINE CIPHER*/"\r\n"; 
+$cipher="aes-256-cbc"; 
+
+/*CREATE A 256 BIT ENCRYPTION KEY*/"\r\n"; 
+$encryption_key=openssl_random_pseudo_bytes(32); 
+/*SAVE $encryption_key SOMEWHERE, LIKE IN A DB*/"\r\n";
+
+/*GENERATE AN IV*/"\r\n"; 
+$iv_size=openssl_cipher_iv_length($cipher); 
+$iv=openssl_random_pseudo_bytes($iv_size); 
+/*SAVE $iv SOMEWHERE, LIKE IN A DB*/"\r\n";
+
+/*ENCRYPT DATA*/"\r\n";  
+$encrypted_data=openssl_encrypt($data, $cipher, $encryption_key, 0, $iv); 
+/*SAVE $encrypted_data SOMEWHERE*/"\r\n";
+?>
