9098ae5abc8b26b21a5a9eb91c6e7368ec4b7ec8
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-11 22:03:03 -0800

    Update csp_reports.js

--- Content-Security-Policy/Reports/csp_reports.js
+++ Content-Security-Policy/Reports/csp_reports.js
@@ -3,10 +3,10 @@
 /*LAST UPDATED: APRIL 11/2022*/
 /*THIS SCRIPT COLLECTS A REPORT OF VIOLATED CSP POLICYS*/
 /*THIS SCRIPT IS ABLE TO COLLECT DATA FROM ERROR OUTPUT, OF THE CSP.*/
-/*SCRIPT CAN BE CALLED WITH A "src" ATTRIBUTE, IN A SCRIPT.*/
-/*SCRIPT WILL NOT LOAD, FROM THE SAME PAGE, WHEN CSP IS SET UP, BECAUSE THAT VIOLATES STANDARD CSP.*/
+/*SCRIPT CAN BE CALLED WITH A "src" ATTRIBUTE, IN A WEBPAGE.*/
+/*SCRIPT WILL NOT LOAD, FROM THE SAME PAGE, WHEN CSP IS SET UP PROPERLY, BECAUSE THAT VIOLATES STANDARD CSP.*/
 /*USAGE:*/
-/*CALL THIS SCRIPT, BEFORE OTHER SCRIPTS, SO IT CAN IDENTIFY PROBLEMS, BEFORE THE PAGE FAILS*/
+/*CALL THIS SCRIPT, BEFORE OTHER SCRIPTS, ON THE PAGE*/
 /*CALL REPORT WITH "violation_report" JS VARIABLE*/
 document.addEventListener("securitypolicyviolation", (e) => {
  /*CHECK IF POLICY IS ENFORCED, OR SET TO REPORT ONLY:*/
@@ -21,23 +21,23 @@ document.addEventListener("securitypolicyviolation", (e) => {
  var originalSource=e.referrer;
  /*VIOLATION SOURCE FILE PATH (E.G.: https://somesite.com/file.php):*/
  var violationFileSource=e.sourceFile;
- /*VIOLATION SOURCE URI (E.G.: https://somesite.com/file.php?this=1&is=1&a=1&uri=1):*/
+ /*VIOLATION SOURCE URI PATH (E.G.: https://somesite.com/file.php?this=1&is=1&a=1&uri=1):*/
  var violationURISource=e.documentURI;
  /*LINE NUMBER OF VIOLATING RESOURCE (IF INLINE)*/
  var violationLineNumber=e.lineNumber;
- /*STATUS CODE OF HTTP REQUEST*/
+ /*HTTP STATUS CODE OF VIOLATION OCCURRENCE*/
  var occurenceStatusCode=e.statusCode;
  
 var violation_report=
-'CSP "enforce" or set to report only?: ' + policyEnforced + '\n' +
-'Policy in place: ' + setPolicy + '\n' +
+'CSP set to "enforce" or "report"?: ' + policyEnforced + '\n' +
+'CSP in place: ' + setPolicy + '\n' +
 'Violated policy: ' + violatedPolicy + '\n' +
 'Violation type: ' + blockedResourceType + '\n' +
-'Violation source (If not inline): ' + originalSource + '\n' +
-'Violation file source: ' + violationFileSource + '\n' +
-'Violation URI source: ' + violationURISource + '\n' +
-'Line of page violation: ' + violationLineNumber + '\n' +
-'Column of page violation: ' + violationColumnNumber + '\n' +
+'Violation source IP/Domain (If not inline): ' + originalSource + '\n' +
+'Violation file source path: ' + violationFileSource + '\n' +
+'Violation URI source path: ' + violationURISource + '\n' +
+'Line number of page violation: ' + violationLineNumber + '\n' +
+'Column number of page violation: ' + violationColumnNumber + '\n' +
 'Blocked resource: ' + blockedResource + '\n' +
-'Occurrence status code: ' + occurenceStatusCode + '\n';
+'HTTP status code of occurrence: ' + occurenceStatusCode + '\n';
 });
