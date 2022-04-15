74eaf629ae00279b74f2a48c431bea591ab7f0a6
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-14 16:38:00 -0800

    Update test.php

--- Encrypt&Decrypt/Variables/test/test.php
+++ Encrypt&Decrypt/Variables/test/test.php
@@ -1,5 +1,10 @@
 <div><h1>Enter some text to encrypt:ncrypt:</h1></div>
-<form method="post" action="encrypt.php">
+<form method="post" action="test.php">
 <input type="text" name="plain_text">
 <input type="submit" value="submit">
 </form>
+<?php
+if(!empty($_POST['plain_text']){
+echo "<div><h1>Encrypted Text:</h1></div>";
+data=$_POST['plain_text'];
+}
