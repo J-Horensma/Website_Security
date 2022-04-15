21048f1dcfb396a70e9ffa6365fad693513654a6
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-14 16:49:44 -0800

    Update test.php

--- Encrypt&Decrypt/Variables/test/test.php
+++ Encrypt&Decrypt/Variables/test/test.php
@@ -5,22 +5,34 @@
 </form>
 <?php
 if(!empty($_POST['plain_text']){
-data=$_POST['plain_text'];
+/*SET "$data" VARIABLE*/"\r\n";
+$data=$_POST['plain_text'];
 
 /*DEFINE CIPHER*/"\r\n"; 
 $cipher="aes-256-cbc"; 
 
 /*GENERATE A 256 BIT ENCRYPTION KEY*/"\r\n"; 
 $encryption_key=openssl_random_pseudo_bytes(32); 
-/*SAVE $encryption_key SOMEWHERE, LIKE IN A DB*/"\r\n";
 
 /*GENERATE AN IV*/"\r\n"; 
 $iv_size=openssl_cipher_iv_length($cipher); 
 $iv=openssl_random_pseudo_bytes($iv_size); 
-/*SAVE $iv SOMEWHERE, LIKE IN A DB*/"\r\n";
 
 /*ENCRYPT DATA*/"\r\n";  
 $encrypted_data=openssl_encrypt($data, $cipher, $encryption_key, 0, $iv); 
-/*SAVE $encrypted_data SOMEWHERE, FOR LATER DECRYPTION*/"\r\n"; 
-echo "<div><h1>Encrypted Text: $encrypted_data</h1></div>";
+ 
+echo "<div><h1>Encrypted text: $encrypted_data</h1></div>";
+
+/*SET VARIABLES*/"\r\n";
+$encryption_key=$encryption_key;
+$iv=$iv;
+$encrypted_data=$encrypted_data;
+
+/*DEFINE CIPHER*/"\r\n"; 
+$cipher = "aes-256-cbc"; 
+
+/*DECRYPT*/"\r\n"; 
+$decrypted_data=openssl_decrypt($encrypted_data, $cipher, $encryption_key, 0, $iv); 
+
+echo "<div><h1>Decrypted text: $decrypted_data</h1></div>";
 }
