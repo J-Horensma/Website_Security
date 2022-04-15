1b786ad6e154786e9639607be222d808d15f15d7
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-14 19:15:03 -0800

    Update AES-256-CBC-decrypt.php

--- Encrypt&Decrypt/Variables/Decrypt/AES-256-CBC-decrypt.php
+++ Encrypt&Decrypt/Variables/Decrypt/AES-256-CBC-decrypt.php
@@ -13,7 +13,7 @@ $encrypted_data='ENCRYPTED DATA';
 /*DEFINE CIPHER*/"\r\n"; 
 $cipher = "aes-256-cbc"; 
 
-/*DECRYPT*/"\r\n"; 
+/*DECRYPT DATA*/"\r\n"; 
 $decrypted_data=openssl_decrypt($encrypted_data, $cipher, $encryption_key, 0, $iv); 
 /*DO SOMETHING WITH "$decrypted_data"*/"\r\n";
 ?>
