4d900748655f192a57de6308eb6e70cce1290ada
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-14 21:51:30 -0800

    Update test.php

--- Headers/Content-Security-Policy/Reports/test/test.php
+++ Headers/Content-Security-Policy/Reports/test/test.php
@@ -1,1 +1,16 @@
+<?php
+/*SET REPORT SCRIPT*/"\r\n";
 
+/*SET CONTENT SECURITY POLICY*/"\r\n";
+if(isset($_SERVER['HTTPS'])) {
+$csp_self_domain="https://".$_SERVER['SERVER_NAME'];
+$csp_self_subdomains="https://".$csp_self_domain;
+}
+else{
+$csp_self_domain="http://".$_SERVER['SERVER_NAME'];
+$csp_self_subdomains="http://".$csp_self_domain;
+}
+/*SET "Content-Security-Policy" HEADER*/"\r\n";
+header("Content-Security-Policy: default-src $csp_self_domain $csp_self_subdomains $csp_trusted");
+
+?>
