eced0f817d246b5ec378c83ea4ef2bdb2b3f3809
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-14 22:39:24 -0800

    Update test.php

--- Headers/Content-Security-Policy/Reports/test/test.php
+++ Headers/Content-Security-Policy/Reports/test/test.php
@@ -1,17 +1,22 @@
 <?php
 /*SET REPORT SCRIPT*/"\r\n";
 echo "<script src='js/csp_report.js'></script>";
+
 /*SET CONTENT SECURITY POLICY*/"\r\n";
+$domain=$_SERVER['SERVER_NAME'];
 if(isset($_SERVER['HTTPS'])) {
 $csp_self_domain="https://".$_SERVER['SERVER_NAME'];
-$csp_self_subdomains="https://".$csp_self_domain;
+$csp_self_subdomains="https://*".$domain;
 }
 else{
 $csp_self_domain="http://".$_SERVER['SERVER_NAME'];
-$csp_self_subdomains="http://".$csp_self_domain;
+$csp_self_subdomains="http://*".$domain;
 }
-echo "<script>alert('This script works inline, because its before the header.');</script>";
+
 /*SET "Content-Security-Policy" HEADER*/"\r\n";
 header("Content-Security-Policy: default-src $csp_self_domain $csp_self_subdomains $csp_trusted");
-echo "<script>alert('This script does not work inline, because its after the header.');</script>";
+
+/*REMOVE THE BELOW STYLE ATTRIBUTE TO NOT TRIGGER A CSP REPORT PAGE*/"\r\n";
+
+echo "<body style='color:blue'>The page loaded!</body>";
 ?>
