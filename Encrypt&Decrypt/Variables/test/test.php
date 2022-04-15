0c8375c485a0d63b5a34734841d378a1a0bed942
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-14 16:40:41 -0800

    Update test.php

--- Encrypt&Decrypt/Variables/test/test.php
+++ Encrypt&Decrypt/Variables/test/test.php
@@ -5,6 +5,22 @@
 </form>
 <?php
 if(!empty($_POST['plain_text']){
-echo "<div><h1>Encrypted Text:</h1></div>";
 data=$_POST['plain_text'];
+
+/*DEFINE CIPHER*/"\r\n"; 
+$cipher="aes-256-cbc"; 
+
+/*GENERATE A 256 BIT ENCRYPTION KEY*/"\r\n"; 
+$encryption_key=openssl_random_pseudo_bytes(32); 
+/*SAVE $encryption_key SOMEWHERE, LIKE IN A DB*/"\r\n";
+
+/*GENERATE AN IV*/"\r\n"; 
+$iv_size=openssl_cipher_iv_length($cipher); 
+$iv=openssl_random_pseudo_bytes($iv_size); 
+/*SAVE $iv SOMEWHERE, LIKE IN A DB*/"\r\n";
+
+/*ENCRYPT DATA*/"\r\n";  
+$encrypted_data=openssl_encrypt($data, $cipher, $encryption_key, 0, $iv); 
+/*SAVE $encrypted_data SOMEWHERE, FOR LATER DECRYPTION*/"\r\n"; 
+echo "<div><h1>Encrypted Text: $encrypted_data</h1></div>";
 }
