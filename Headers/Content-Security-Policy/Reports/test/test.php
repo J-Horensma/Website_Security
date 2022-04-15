797c8329eccd7c4a24fc58ae7df93ca9250acdc9
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-14 22:00:33 -0800

    Update test.php

--- Headers/Content-Security-Policy/Reports/test/test.php
+++ Headers/Content-Security-Policy/Reports/test/test.php
@@ -1,6 +1,6 @@
 <?php
 /*SET REPORT SCRIPT*/"\r\n";
-
+echo "<script src='js/csp_report.js'></script>";
 /*SET CONTENT SECURITY POLICY*/"\r\n";
 if(isset($_SERVER['HTTPS'])) {
 $csp_self_domain="https://".$_SERVER['SERVER_NAME'];
@@ -10,7 +10,8 @@ else{
 $csp_self_domain="http://".$_SERVER['SERVER_NAME'];
 $csp_self_subdomains="http://".$csp_self_domain;
 }
+echo "<script>alert('This script works inline, because its before the header.');</script>";
 /*SET "Content-Security-Policy" HEADER*/"\r\n";
 header("Content-Security-Policy: default-src $csp_self_domain $csp_self_subdomains $csp_trusted");
-
+echo "<script>alert('This script does not work inline, because its after the header.');</script>";
 ?>
