57e62e62dd610d4804c6c77d4ae17465af318c2a
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-14 18:39:20 -0800

    Update AES-256-CBC-encrypt.php

--- Encrypt&Decrypt/Files/Client_File_Upload/Encrypt/AES-256-CBC-encrypt.php
+++ Encrypt&Decrypt/Files/Client_File_Upload/Encrypt/AES-256-CBC-encrypt.php
@@ -2,7 +2,7 @@
 /*WHEN A FILE IS UPLOADED TO A SERVER, UNLIKE USUAL INPUT,*/"\r\n";
 /*INSTEAD OF BEING STORED IN THE "$_POST['name']" VARIABLE,*/"\r\n";
 /*IT'S STORED IN A TEMP FILE, ON THE SERVER (BY STANDARD AND DEFAULT).*/"\r\n";
-/*THE TEMP FILE NAME IS DEFINED BY THE "$_FILES['tmp_name']"*/"\r\n";
+/*THE TEMP FILE NAME IS DEFINED BY THE "$_FILES['name']['tmp_name']"*/"\r\n";
 
 /*SET VARIABLE, FOR DATA TO ENCRYPT, FROM THE TEMP FILE CONTENTS*/"\r\n";
 $data=file_get_contents($_FILES['file']['tmp_name']);
