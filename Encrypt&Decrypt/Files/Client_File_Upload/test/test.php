884e1a13b01189f0e14306e2234465f2fee5b278
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-14 18:11:51 -0800

    Update test.php

--- Encrypt&Decrypt/Files/Client_File_Upload/test/test.php
+++ Encrypt&Decrypt/Files/Client_File_Upload/test/test.php
@@ -1,3 +1,4 @@
+<div><h1>Choose file to encrypt:</h1></div>
 <form method="post" action="test.php" enctype="multipart/form-data">
 <input type="file" name="file">
 <input type="submit" value="Upload"/>
