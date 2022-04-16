237877f4b6c1c6624f2ea079228ca259c80dc401
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-15 18:56:29 -0800

    Update AES-256-CBC-encrypt.php

--- Encrypt_&_Decrypt/File_Upload/Encrypt/AES-256-CBC-encrypt.php
+++ Encrypt_&_Decrypt/File_Upload/Encrypt/AES-256-CBC-encrypt.php
@@ -1,1 +1,25 @@
+<?php
+/*WHEN A FILE IS UPLOADED TO A SERVER, UNLIKE USUAL INPUT,*/"\r\n";
+/*INSTEAD OF BEING STORED IN THE "$_POST['name']" VARIABLE,*/"\r\n";
+/*IT'S STORED IN A TEMP FILE, ON THE SERVER (BY STANDARD AND DEFAULT).*/"\r\n";
+/*THE TEMP FILE NAME IS DEFINED BY THE "$_FILES['FILE_INPUT_NAME']['tmp_name']"*/"\r\n";
 
+/*SET VARIABLE, FOR DATA TO ENCRYPT, FROM THE TEMP FILE CONTENTS*/"\r\n";
+$data=file_get_contents($_FILES['FILE_INPUT_NAME']['tmp_name']);
+
+/*DEFINE CIPHER*/"\r\n"; 
+$cipher="aes-256-cbc"; 
+
+/*GENERATE A 256 BIT ENCRYPTION KEY*/"\r\n"; 
+$encryption_key=openssl_random_pseudo_bytes(32); 
+/*SAVE $encryption_key SOMEWHERE*/"\r\n";
+
+/*GENERATE AN IV*/"\r\n"; 
+$iv_size=openssl_cipher_iv_length($cipher); 
+$iv=openssl_random_pseudo_bytes($iv_size); 
+/*SAVE $iv SOMEWHERE*/"\r\n";
+
+/*ENCRYPT DATA*/"\r\n";  
+$encrypted_data=openssl_encrypt($data, $cipher, $encryption_key, 0, $iv); 
+/*SAVE "$encrypted_data" SOMEWHERE*/"\r\n";
+?>
