960bc8163c2158c62ae32797f004e6a4213324ec
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-15 18:20:51 -0800

    Update AES-256-CBC-encrypt.php

--- Encrypt_&_Decrypt/Variables/Encrypt/AES-256-CBC-encrypt.php
+++ Encrypt_&_Decrypt/Variables/Encrypt/AES-256-CBC-encrypt.php
@@ -1,1 +1,26 @@
+<?php
+/*THIS SCRIPT ENCRYPTS DATA, WITH AES 256 BIT CBC ENCRYPTION (GOVERNMENT GRADE)*/
+/*REF: https://www.phpcluster.com/aes-encryption-and-decryption-in-php/*/"\r\n";
+/*OPENSSL WORKS BY DEFAULT, IN MODERN PHP VERSIONS*/"\r\n";
+/*WORKS WITH USER INPUT VARIABLES, SEE "FILES" SECTION, FOR LOCAL FILE OR FILE UPLOAD ENCRYPTION*/"\r\n";
+/*THE KEY AND IV, ARE REQUIRED BY THE DECRYPTION SCRIPT, SO IT CAN DECRYPT THE ENCRYPTED DATA*/"\r\n";
 
+/*SET VARIABLE FOR DATA TO ENCRYPT*/"\r\n";
+$data='TEXT OR FILE TO ENCRYPT';
+
+/*DEFINE CIPHER*/"\r\n"; 
+$cipher="aes-256-cbc"; 
+
+/*GENERATE A 256 BIT ENCRYPTION KEY*/"\r\n"; 
+$encryption_key=openssl_random_pseudo_bytes(32); 
+/*SAVE $encryption_key SOMEWHERE*/"\r\n";
+
+/*GENERATE AN IV*/"\r\n"; 
+$iv_size=openssl_cipher_iv_length($cipher); 
+$iv=openssl_random_pseudo_bytes($iv_size); 
+/*SAVE $iv SOMEWHERE*/"\r\n";
+
+/*ENCRYPT DATA*/"\r\n";  
+$encrypted_data=openssl_encrypt($data, $cipher, $encryption_key, 0, $iv); 
+/*SAVE "$encrypted_data" SOMEWHERE*/"\r\n";
+?>
