0862609c45a76a0e004095aaf466b6cdd432d1d8
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2022-04-16 18:58:40 -0800

    Update test.php

--- Headers/Content-Security-Policy/Reports/test/test.php
+++ Headers/Content-Security-Policy/Reports/test/test.php
@@ -6,11 +6,12 @@ echo "<script src='js/csp_report.js'></script>";
 $domain=$_SERVER['SERVER_NAME'];
 if(isset($_SERVER['HTTPS'])) {
 $csp_self_domain="https://".$_SERVER['SERVER_NAME'];
+/*www, WILL BE TREATED AS A SUBDOMAIN*/"\r\n";
 $csp_self_subdomains="*".$domain;
 }
 else{
 $csp_self_domain="http://".$_SERVER['SERVER_NAME'];
-$csp_self_subdomains="http://*".$domain;
+$csp_self_subdomains="*".$domain;
 }
 
 /*SET "Content-Security-Policy" HEADER*/"\r\n";
